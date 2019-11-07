8885b43f772ba0ef831248073c72af0ca7b5aa6a
Jisub Kim <plumssi9@gmail.com>
2019-11-07 19:05:58 +0900

    make parse_article() and parse_comment()

--- dcCrawler/spiders/dcSpider.py
+++ dcCrawler/spiders/dcSpider.py
@@ -1,31 +1,44 @@
 #!-*- coding:utf-8 -*-
 import scrapy
 import time
+from urllib.parse import urlparse, parse_qs
 from dcCrawler.items import DccrawlerItem
 
 class dcCrawlerSpider(scrapy.Spider):
     name = "dcCrawler"
-    # start_urls = [
-    #     "https://gall.dcinside.com/board/lists/?id=leagueoflegends3&page={}",
-    #     # "https://gall.dcinside.com/board/lists/?id=superidea"
-    #     ]
     def start_requests(self):
-        for i in range(2):
+        for i in range(1):
             yield scrapy.Request(
                 "https://gall.dcinside.com/board/lists/?id=leagueoflegends3&page={}".format(i+1), self.parse_url )
 
     def parse_url(self, response):
         for article in response.xpath('//*[@id="container"]/section[1]/article[2]/div[2]/table/tbody/tr/td[2]'):
-            link = article.xpath("a[1]/@href").extract()[0]
-            print(response.urljoin(link))
-            request = scrapy.Request(
-                    response.urljoin(link), # article link
+            query = article.xpath("a[1]/@href").extract()[0]
+            print (query)
+            time.sleep(1)
+            yield scrapy.Request(
+                    response.urljoin(query), # article link
                     callback = self.parse_article
                 )
-            sleep(1)
-            # print(article.xpath("a[2]/@href").extract()) # comment
-            yield request
+        
 
     def parse_article(self, response):
-        title = response.xpath('//*[@id="container"]/section/article[2]/div[1]/header/div/h3/span[2]') 
-        print(title)
+        title = response.xpath('//*[@id="container"]/section/article[2]/div[1]/header/div/h3/span[2]/text()').extract() 
+        _query = parse_qs( urlparse(response.url).query )
+        _query['title'] = title
+
+        data = {
+            "id": _query['id'], "no": _query['no'], "cpage": _query['page'], "managerskill":"", "del_scope": "1", "csort": ""
+            }
+        yield scrapy.FormRequest(
+            "https://m.dcinside.com/ajax/response-comment",
+            formdata  = data, callback = self.parse_comment,
+            cb_kwargs = _query
+        )
+    
+
+    def parse_comment(self, response, id, no, page, title):
+        print(id, no, title)
+        print(response.xpath('/html/body/ul/li/p/text()').extract())
+        
+        
